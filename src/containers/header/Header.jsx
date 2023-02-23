fe2546f6dc7023572c7ba366a49676883e1a683b
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-02-23 13:15:01 +0100

    Header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,8 +3,15 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>
-        Header
+    <div className="gpt3__header section_padding" id="home">
+        <div className="gpt3__header-content">
+          <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
+
+          <div className="gpt3__header-content__input">
+            <input type="email" placeholder="Your Email Adress" />
+            <button type="button">Get Started</button>
+          </div>
+        </div>
     </div>
   )
 }
