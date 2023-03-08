fff42e57512a96e29811a9a4a15eb24e482492a7
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-08 22:25:41 +0100

    Debugging Header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,8 +3,7 @@ import './header.css';
 import people from '../../assets/people.png';
 import ai from '../../assets/ai.png';
 
-const Header = () => {
-  return (
+const Header = () => (
     <div className="gpt3__header section_padding" id="home">
         <div className="gpt3__header-content">
           <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
@@ -19,13 +18,12 @@ const Header = () => {
             <img src={people} alt="people" />
             <p>1,600 people requested access a visit in last 24 hours</p>
           </div>
+        </div>
 
           {/* <div className="gpt3__header-image">
             <img src={ai} alt="ai" />
           </div> */}
-        </div>
     </div>
-  )
-}
+);
 
 export default Header
