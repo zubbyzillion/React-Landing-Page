50d95ec1d9227c20c6254b35bed0bd4518c67122
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-10 19:18:26 +0100

    Fixed Padding

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './header.css';
 import people from '../../assets/people.png';
 import ai from '../../assets/ai.png';
+import './header.css';
 
 const Header = () => (
-    <div className="gpt3__header section_padding" id="home">
+    <div className="gpt3__header section__padding" id="home">
         <div className="gpt3__header-content">
           <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
           <p>Yet bed any for traveling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
