ec58fd58d15609e71038e3c28aa9ea0017f20fb9
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-11 22:22:21 +0100

    Features Section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -3,13 +3,13 @@ import './feature.css';
 
 const Feature = ({ title, text }) => {
   return (
-    <div className="gpt3__features-container__features">
+    <div className="gpt3__features-container__feature">
         <div className="gpt3__features-container__feature-title">
           <div />
           <h1>{title}</h1>
         </div>
         <div className="gpt3__features-container_feature-text">
-          {text}
+          <p>{text}</p>
         </div>
     </div>
   )
