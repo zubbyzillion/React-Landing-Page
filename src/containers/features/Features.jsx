28741664568a7e322175291a46fa83b902d0fb47
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-11 22:38:01 +0100

    Features Section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Feature from '../../components/feature/Feature';
 import './features.css';
 
 const Features = () => {
   return (
-    <div>
-        Features
+    <div className="gpt3__features section__padding" id="features">
+        <div className="gpt3__features-heading">
+          <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
+          <p>Request Early Access to Get Started</p>
+        </div>
     </div>
   )
 }
 
-export default Features
+export default Features;
