c5729caa8d95e1ece721077bf4506ad5540bb427
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-13 16:21:13 +0100

    Article Styling

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -11,13 +11,13 @@ const Blog = () => {
         </div>
         <div className="gpt3__blog-container">
           <div className="gpt3__blog-container__groupA">
-            <Article imgUrl={blog01} />
+            <Article imgUrl={blog01} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us explore how it is?" />
           </div>
           <div className="gpt3__blog-container__groupB">
-            <Article imgUrl={blog02} />
-            <Article imgUrl={blog03} />
-            <Article imgUrl={blog04} />
-            <Article imgUrl={blog05} />
+            <Article imgUrl={blog02} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us explore how it is?" />
+            <Article imgUrl={blog03} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us explore how it is?" />
+            <Article imgUrl={blog04} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us explore how it is?" />
+            <Article imgUrl={blog05} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us explore how it is?" />
           </div>
         </div>
     </div>
