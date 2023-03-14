d5796bc53a9e6013a6347faab7fd0939bb5b0601
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-14 09:29:12 +0100

    Footer Section

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -3,8 +3,9 @@ import './footer.css';
 
 const Footer = () => {
   return (
-    <div>
-        Footer
+    <div className="gpt3__footer section__padding">
+        <div className="gpt3__footer-heading"></div>
+        <h1 className="gradient__text">Do you want to step in to the future before others</h1>
     </div>
   )
 }
